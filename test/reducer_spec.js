32dba7dadf69b5deebc4dba75d26a6755d6a7c4f
Andres Osorio <androideosorio@me.com>
2016-07-22 13:35:21 -0500

    feat(app-reducer): reducer can handle all of the current app's actions

--- test/reducer_spec.js
+++ test/reducer_spec.js
@@ -1,17 +1,45 @@
 // app reducer spec
-import { Map, fromJS } from 'immutable';
+import { List, Map, fromJS } from 'immutable';
 import { expect } from 'chai';
 
 import reducer from '../src/reducer';
 
 // describe the main reducer behavior
 describe('Application Reducer', () => {
-
-  it('handles SET_ENTRIES', () => {
+  // reducer can handle SET_ENTRIES Action
+  it('handles SET_ENTRIES action', () => {
     const initialState = new Map();
     const action = { type: 'SET_ENTRIES', entries: ['Kill Bill'] };
     const nextState = reducer(initialState, action);
 
     expect(nextState).to.have.property('entries').that.includes('Kill Bill');
+  });
+
+  // reducer can handle Next Actions
+  it('handles NEXT action', () => {
+    const initialState = fromJS({ entries: ['Kill Bill', 'Pulp Fiction', 'Grindhouse'] });
+    const action = { type: 'NEXT' };
+    const nextState = reducer(initialState, action);
+
+    expect(nextState)
+      .to.have.property('entries')
+      .that.equals(List.of('Grindhouse'));
+    expect(nextState)
+      .to.have.deep.property('vote.pair')
+      .that.includes('Kill Bill', 'Pulp Fiction');
+  });
+
+  // reducer can handle VOTE actions
+  it('handles VOTE action', () => {
+    const initialState = fromJS({ vote: { pair: ['Kill Bill', 'Pulp Fiction'] }, entries: [] });
+    const action = { type: 'VOTE', entry: 'Kill Bill' };
+    const nextState = reducer(initialState, action);
+
+    expect(nextState)
+      .to.have.deep.property('vote.tally')
+      .that.includes.keys('Kill Bill');
+    expect(nextState)
+      .to.have.deep.property(['vote', 'tally', 'Kill Bill'])
+      .that.equals(1);
   });
 });
